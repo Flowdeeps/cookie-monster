2f229eb72a2037edf2ff9aee90037d307e2be92c
Pete Haughie <pete.haughie@sourcefabric.org>
2016-05-20 20:07:26 +0100

    Made the fake website a bit more appealing and tidied all the js up into nice little reusable blocks. I may even actually put some cookie writing stuff in there.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,19 +1,80 @@
 window.onload = function() {
 
-  var radYes = document.getElementById( "yes" );
-  var radNo = document.getElementById( "no" );
-  var reaction = document.getElementById( "cookieStatus" );
-  radYes.onchange = function() {
+  // set up the objects
+  var cookiePerms = document.getElementById( 'cookies_permissions' ),
+      closeButt = document.getElementById( 'close' );
+      radYes = document.getElementById( 'yes' ),
+      radNo = document.getElementById( 'no' ),
+      reaction = document.getElementById( 'cookie_status' );
+
+  // if the user says yes
+  var doSuccess = function() {
     if ( reaction.classList.contains( 'failure' ) ) {
       reaction.classList.remove( 'failure' )
     }
     reaction.classList.add( 'success' );
   }
-  radNo.onchange = function() {
+
+  // if the user says no
+  var doFailure = function() {
     if ( reaction.classList.contains( 'success' ) ) {
       reaction.classList.remove( 'success' );
     }
     reaction.classList.add( 'failure' );
+  }
+
+  // this is where any cookie writing code will go
+  var makeCookie = function(){
+    console.log( "Make a cookie here" );
+  }
+
+  radYes.onchange = function() {
+    doSuccess();
+    hidePerms();
+    makeCookie();
+    setTimeout( function() {
+      hideReaction();
+    }, 3000);
+  }
+
+  radNo.onchange = function() {
+    doFailure();
+    hidePerms();
+    setTimeout( function() {
+      hideReaction();
+    }, 5000);
+  }
+
+  var hideReaction = function() {
+    document.body.removeChild( reaction );
+  }
+
+  var hidePerms = function() {
+    document.body.removeChild( cookiePerms );
+  }
+
+  var hidePermsAndReaction = function() {
+    setTimeout( function() {
+        hidePerms();
+        hideReaction();
+    }, 3000);
+  }
+
+  var disappoint = function() {
+    alert( "I'm sorry, we're all out of cookies." );
+  }
+  var cookieSplode = document.getElementById( 'cookiesplosion' );
+  cookieSplode.onclick = function() {
+    disappoint();
+  };
+
+  closeButt.onclick = function() {
+    doSuccess();
+    makeCookie();
+    hidePerms();
+    setTimeout( function() {
+      hideReaction();
+    }, 3000);
   }
 
 };
